19785bbb578020aa1959471761b1253501e64176
Shiro <khoirul.20998@gmail.com>
2019-01-27 21:13:40 +0700

    Masih dalam tahap perbaikan tanggal 27 Januari 2019

--- latihan_3/controller.js
+++ latihan_3/controller.js
@@ -2,6 +2,9 @@
 var response = require('./res');
 var qb = require('./conn');
 var Joi = require('joi');
+var fs = require('fs');
+// Path
+var path = require('path');
 // Mengambil data
 exports.uploads = (req, res) => {
     qb.get('tuplod', (err, output) => {
@@ -14,19 +17,19 @@ exports.uploads = (req, res) => {
         }
     })
 }
-// exports.findUsers = (req, res) => {
-//     qb.where({
-//         id: req.params.user_id
-//     }).get('person', (err, output) => {
-//         if (err) {
-//             response.err({
-//                 message: err
-//             }, res);
-//         } else {
-//             response.ok(output, res);
-//         }
-//     });
-// }
+exports.findUsers = (req, res) => {
+    qb.where({
+        id: req.params.user_id
+    }).get('person', (err, output) => {
+        if (err) {
+            response.err({
+                message: err
+            }, res);
+        } else {
+            response.ok(output, res);
+        }
+    });
+}
 // exports.updateUser = (req, res) => {
 //  const {error} = validasi(req.body);
 //     if (error) return response.err({
@@ -62,33 +65,53 @@ exports.uploads = (req, res) => {
 //         }
 //     });
 // }
-// exports.deleteUser = (req, res) => {
-//     var whereNya = {
-//         id: req.params.user_id
-//     };
-//     qb.where(whereNya).delete('person', (err, output) => {
-//         if (err) {
-//             response.err({
-//                 message: err
-//             }, res);
-//         } else {
-//             var {
-//                 affectedRows
-//             } = output;
-//             if (affectedRows == 1) {
-//                 var hasil = {
-//                     message: "Berhasil menghapus person",
-//                 }
-//                 response.ok(hasil, res);
-//             } else {
-//                 var hasil = {
-//                     message: "Gagal menghapus person",
-//                 }
-//                 response.err(hasil, res);
-//             }
-//         }
-//     });
-// }
+exports.deleteUpload = (req, res) => {
+    qb.where({
+        'id': req.params.tid
+    }).get('tuplod', (err, output) => {
+        if (err) {
+            response.err({
+                message: err
+            }, res);
+        } else {
+            if (output.length >= 1) {
+                // console.log(output[0].image);
+                if (fs.existsSync(path.join(__dirname + '/public/images/' + output[0].image))) {
+                    fs.unlink(path.join(__dirname + '/public/images/' + output[0].image));
+                }
+                var where = {
+                    'id': req.params.tid
+                }
+                qb.where(where).delete('tuplod', (errNya, output) => {
+                    if (errNya) {
+                        response.err({
+                            message: errNya
+                        }, res);
+                    } else {
+                        var {
+                            affectedRows
+                        } = output;
+                        if (affectedRows == 1) {
+                            var hasil = {
+                                message: "Berhasil menghapus T Upload",
+                            }
+                            response.ok(hasil, res);
+                        } else {
+                            var hasil = {
+                                message: "Gagal menghapus T Upload",
+                            }
+                            response.err(hasil, res);
+                        }
+                    }
+                });
+            } else {
+                response.err({
+                    message: 'Gagal menghapus T Upload'
+                }, res);
+            }
+        }
+    });
+}
 exports.createUpload = (req, res) => {
     if (req.file != null) {
         var data = {
@@ -114,13 +137,6 @@ exports.createUpload = (req, res) => {
             message: "Gagal menambah T Uploads"
         }, res);
     }
-}
-const validasi = (belajar) => {
-    const schema = {
-        first_name: Joi.string().min(3).required(),
-        last_name: Joi.string().min(3).required()
-    };
-    return Joi.validate(belajar, schema);
 }
 exports.index = (req, res) => {
     response.ok('Selamat datang di belajar RestApi dengan Express', res);
--- latihan_3/upload.js
+++ latihan_3/upload.js
@@ -1,17 +1,16 @@
 // Multer
-    var multer = require('multer');
-    // Path
-    var path = require('path');
-    //set storage engine
-    const storage = multer.diskStorage({
-        destination: path.join(__dirname + '/public/images/'),
-        filename: function(req, file, cb) {
-            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-        }
-    });
-    //init upload
-    const upload = multer({
-        storage: storage
-    });
-
-    module.exports = upload;
+var multer = require('multer');
+// Path
+var path = require('path');
+//set storage engine
+const storage = multer.diskStorage({
+    destination: path.join(__dirname + '/public/images/'),
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+//init upload
+const upload = multer({
+    storage: storage
+});
+module.exports = upload;
